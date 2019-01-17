74cdf72b06618a6d252afdafd8ec86d4573d0293
zhengxi <xi.zheng@changhong.com>
2019-01-17 22:12:03 +0800

    kkk

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -371,7 +371,7 @@
     <transition :name="transition">
       <ul ref="dropdownMenu" v-if="dropdownOpen" class="dropdown-menu" :style="{ 'max-height': maxHeight }" role="listbox" @click="onMousedown">
         <li role="option" v-for="(option, index) in filteredOptions" v-bind:key="index" :class="{ active: isOptionSelected(option), highlight: index === typeAheadPointer }">
-          <a @click.prevent="select(option, index)">
+          <a @click="select(option, index)">
           <slot name="option" v-bind="(typeof option === 'object')?option:{[label]: option}">
             {{ getOptionLabel(option) }}
           </slot>
